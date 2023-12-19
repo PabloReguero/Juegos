8a5fc8aed75b582f77e1f037fbd4680c05934672
PabloReguero <146989229+PabloReguero@users.noreply.github.com>
2023-12-19 09:51:29 +0100

    Update BlackJack_sin_imagen.py

--- BlackJack_sin_imagen.py
+++ BlackJack_sin_imagen.py
@@ -1,1 +1,10 @@
-print('Voy a crear el BlackJack')
+# Reglas:
+#  - Todas las cartas valen el valor de la misma, menos el As que vale o 1 o 11 y las figuras que valen 10
+#  - La primera carta de los jugadores y del dealer no se muestran
+#  - Introcue la cantidad total con la que quieras jugar, que no es lo mismo con lo que quieras apostar
+#  - Aumenta o disminuye esa cantidad en función de tus beneficios o pérdidas
+#  - Siempre empieza apostando el jugador1 o jugador_x
+#  - El jugador_dealer cuando la suma de las cartas mostradas sea 16 o más se planta automáticamente
+
+#  - Cuando un jugador llegue a 0. print(f'{juagdor_x} vete de la mesa moroso de mierda')
+#  - El dealer tiene 1000000. Si llega a 0. print(f'{jugador_dealer} ha recibidio una paliza por no pagar a los jugadores de la mesa)
