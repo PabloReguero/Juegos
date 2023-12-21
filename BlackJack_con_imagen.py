ccb9904b658ea5fa01f0abe50296f5d21f94383e
PabloReguero <146989229+PabloReguero@users.noreply.github.com>
2023-12-21 15:49:29 +0100

    Update BlackJack_con_imagen.py

--- BlackJack_con_imagen.py
+++ BlackJack_con_imagen.py
@@ -1,10 +1,229 @@
-# Reglas:
-#  - Todas las cartas valen el valor de la misma, menos el As que vale o 1 o 11 y las figuras que valen 10
-#  - La primera carta de los jugadores y del dealer no se muestran
-#  - Introcue la cantidad total con la que quieras jugar, que no es lo mismo con lo que quieras apostar
-#  - Aumenta o disminuye esa cantidad en función de tus beneficios o pérdidas
-#  - Siempre empieza apostando el jugador1 o jugador_x
-#  - El jugador_dealer cuando la suma de las cartas mostradas sea 16 o más se planta automáticamente
+import tkinter as tk
+from tkinter import PhotoImage, messagebox
+import random
 
-#  - Cuando un jugador llegue a 0. print(f'{juagdor_x} vete de la mesa moroso de mierda')
-#  - El dealer tiene 1000000. Si llega a 0. print(f'{jugador_dealer} ha recibidio una paliza por no pagar a los jugadores de la mesa)
+class Card:
+    def __init__(self, suit: str, value: int):
+        self.suit = suit
+        self.value = value        
+        pass
+
+    def get_numeric_value(self) -> int:
+        if self.value in ['K', 'Q', 'J']:
+            return 10
+        elif self.value == 'A':
+            return 11
+        else:
+            return int(self.value)
+    pass
+
+    def get_image(self):
+        return f"img/{self.value}_of_{self.suit}.png"        
+    pass
+
+class Deck:
+    def __init__(self, suits = [], values = []):
+        self.cards = []
+        for value in values:
+            for suit in suits:
+                self.cards.append(Card(suit,value))
+        pass
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+        pass
+
+    def deal(self)-> Card:
+        if not self.cards:
+            raise ValueError("Deck is empty")
+        return self.cards.pop()
+    pass
+
+class EnglishDeck(Deck):
+    def __init__(self):
+        suits = ['hearts', 'diamonds', 'clubs', 'spades']
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        super().__init__(suits, values)
+        pass
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+        pass
+
+    def add_card(self, card: Card):
+        self.cards.append(card)
+        pass
+
+    def value(self)->int:
+        total_value = sum(card.get_numeric_value() for card in self.cards)
+        num_aces = sum(1 for card in self.cards if card.value == 'A')
+
+        while total_value > 21 and num_aces:
+            total_value -= 10
+            num_aces -= 1
+
+        return total_value
+    pass
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.hand = Hand()
+        pass
+
+class BlackjackGame:
+    def __init__(self):
+        self.player = Player("Player")
+        self.dealer = Player("Dealer")
+        self.deck = EnglishDeck()
+        self.deck.shuffle()
+        pass
+
+    def start_game(self):
+        self.player.hand = Hand()
+        self.dealer.hand = Hand()
+
+        for i in range (2):
+            self.player.hand.add_card(self.deck.deal())
+            self.dealer.hand.add_card(self.deck.deal())
+        pass
+
+    def hit(self)-> bool:
+        self.player.hand.add_card(self.deck.deal())
+        return self.player.hand.value() > 21
+    pass
+
+    def dealer_hit(self) -> bool:      
+        while self.dealer.hand.value() < 17:
+            self.dealer.hand.add_card(self.deck.deal())      
+            return self.dealer.hand.value() <= 21
+        pass
+
+    def determine_winner(self):
+        player_value = self.player.hand.value()
+        dealer_value = self.dealer.hand.value()
+
+        if player_value > 21:
+            return "You've busted! The house wins."
+
+        if dealer_value > 21:
+            return "Dealer busts! You win."
+
+        if player_value > dealer_value:
+            return "You win!"
+        elif dealer_value > player_value:
+            return "Dealer wins."
+        else:
+            return "It's a tie!"
+    pass
+
+# The GUI code is provided, so students don't need to modify it
+class BlackjackGUI:
+    def __init__(self, game):
+        self.game = game
+
+        self.root = tk.Tk()
+        self.root.title("Blackjack")
+
+        # Frames for the player and the dealer
+        self.player_frame = tk.Frame(self.root)
+        self.player_frame.pack(side=tk.LEFT, padx=10)
+
+        self.deck_frame = tk.Frame(self.root)
+        self.deck_frame.pack(side=tk.LEFT, padx=10)
+
+        self.dealer_frame = tk.Frame(self.root)
+        self.dealer_frame.pack(side=tk.RIGHT, padx=10)
+
+        # "Stand" button
+        self.btn_stand = tk.Button(self.deck_frame, text="Stand", command=self.handle_stand, state=tk.NORMAL)
+        self.btn_stand.pack(side=tk.BOTTOM)
+
+        self.start_game()
+
+    def start_game(self):
+        self.game.start_game()
+        self.update_interface()
+
+    def handle_hit(self, event):
+        if self.game.hit():
+            self.update_interface()
+            self.end_game("You've busted! The house wins.")
+            return
+        self.update_interface()
+
+    def handle_stand(self):
+        self.btn_stand.config(state=tk.DISABLED)
+        while self.game.dealer_hit():
+            self.update_interface()
+        self.end_game(self.game.determine_winner())
+
+    def update_interface(self):
+        # Remove all widgets from player, deck, and dealer frames
+        for widget in self.player_frame.winfo_children():
+            widget.destroy()
+        
+        for widget in self.deck_frame.winfo_children():
+            widget.destroy()
+
+        for widget in self.dealer_frame.winfo_children():
+            widget.destroy()
+
+        # Player's cards
+        player_previous_frame = tk.Frame(self.player_frame)
+        player_previous_frame.pack(side=tk.LEFT, pady=10)
+
+        for card in self.game.player.hand.cards[:-1]:  # All cards except the last one
+            img = PhotoImage(file=card.get_image())
+            img = img.subsample(3, 3)  # Resize the image (adjust according to your preference)
+            lbl = tk.Label(player_previous_frame, image=img)
+            lbl.image = img
+            lbl.pack(side=tk.TOP, pady=5)  # Add vertical space between cards
+
+        # The last card of the player in the center
+        last_card = self.game.player.hand.cards[-1]
+        img = PhotoImage(file=last_card.get_image())
+        lbl = tk.Label(self.player_frame, image=img)
+        lbl.image = img
+        lbl.pack(side=tk.LEFT, padx=10)  # Center the last card horizontally a bit more
+        
+        # Deck in the middle
+        img = PhotoImage(file="img/card_back_01.png")
+        lbl = tk.Label(self.deck_frame, image=img, cursor="hand2")
+        lbl.image = img
+        lbl.pack(side=tk.TOP, padx=10)
+        lbl.bind("<Button-1>", self.handle_hit)
+        
+        # "Stand" button below the deck
+        self.btn_stand = tk.Button(self.deck_frame, text="Stand", command=self.handle_stand, state=tk.NORMAL)
+        self.btn_stand.pack(side=tk.BOTTOM)
+
+        # Dealer's cards
+        dealer_previous_frame = tk.Frame(self.dealer_frame)
+        dealer_previous_frame.pack(side=tk.RIGHT, pady=10)
+
+        for card in self.game.dealer.hand.cards[:-1]:  # All cards except the last one
+            img = PhotoImage(file=card.get_image())
+            img = img.subsample(3, 3)  # Resize the image (adjust according to your preference)
+            lbl = tk.Label(dealer_previous_frame, image=img)
+            lbl.image = img
+            lbl.pack(side=tk.TOP, pady=5)  # Add vertical space between cards
+
+        # The last card of the dealer in the center
+        last_card = self.game.dealer.hand.cards[-1]
+        img = PhotoImage(file=last_card.get_image())
+        lbl = tk.Label(self.dealer_frame, image=img)
+        lbl.image = img
+        lbl.pack(side=tk.RIGHT, padx=10)  # Center the last card horizontally a bit more
+
+    def end_game(self, message):
+        messagebox.showinfo("Result", message)
+        self.root.quit()
+
+    def run(self):
+        self.root.mainloop()
+if __name__ == "__main__":
+    game_logic = BlackjackGame()
+    app = BlackjackGUI(game_logic)
+    app.run()
